e895fd388aa434683bef16afab97dc444a493580
bennymeade <brendan_meade@yahoo.co.uk>
2021-12-15 21:55:44 +0000

    check in

--- create-env-var.js
+++ create-env-var.js
@@ -4,8 +4,8 @@ const { exec } = require('child_process');
 const resolvePercyTokens = () => {
   if (process.env.PUBLICATION_UNDER_TEST.includes('www')) {
     exec(
-        `echo $PERCY_TOKEN_X`,
-        { env: { ...process.env, PERCY_TOKEN_X: `734573428736548273648723` } },
+        `echo $PERCY_TOKEN`,
+        { env: { ...process.env, PERCY_TOKEN: `734573428736548273648723` } },
         (error, stdout, stderr) => {
           if (error) {
             console.error(`exec error: ${error}`);
