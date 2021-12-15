edca380c9b5aaae8206933df8c750cfdf5e131e4
bennymeade <brendan_meade@yahoo.co.uk>
2021-12-15 22:51:25 +0000

    check in

--- create-env-var.js
+++ create-env-var.js
@@ -3,18 +3,29 @@ const { exec } = require('child_process');
 
 const resolvePercyTokens = () => {
   if (process.env.PUBLICATION_UNDER_TEST.includes('www')) {
+    // exec(
+    //     `echo $PERCY_TOKEN`,
+    //     { env: { ...process.env, PERCY_TOKEN: `734573428736548273648723` } },
+    //     (error, stdout, stderr) => {
+    //       if (error) {
+    //         console.error(`exec error: ${error}`);
+    //         return;
+    //       }
+    //       console.log(`stdout: ${stdout}`);
+    //       console.error(`stderr: ${stderr}`);
+    //     }
+    //   );
     exec(
-        `echo $PERCY_TOKEN`,
-        { env: { ...process.env, PERCY_TOKEN: `734573428736548273648723` } },
-        (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
-          console.log(`stdout: ${stdout}`);
-          console.error(`stderr: ${stderr}`);
+      `export PERCY_TOKEN=${345634756384757760000000}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
         }
-      );
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      }
+    );
   }
 };
 resolvePercyTokens();
