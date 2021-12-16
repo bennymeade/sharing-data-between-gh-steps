7425cffd10247de96785bb5ea145ac290b528af6
bennymeade <brendan_meade@yahoo.co.uk>
2021-12-16 13:28:51 +0000

    check in

--- create-env-var.js
+++ create-env-var.js
@@ -3,29 +3,29 @@ const { exec } = require('child_process');
 
 const resolvePercyTokens = () => {
   if (process.env.PUBLICATION_UNDER_TEST.includes('www')) {
-    exec(
-        `set`,
-        { env: { ...process.env, PERCY_TOKEN: `734573428736548273648723` } },
-        (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
-          console.log(`stdout: ${stdout}`);
-          console.error(`stderr: ${stderr}`);
-        }
-      );
     // exec(
-    //   `export PERCY_TOKEN=${345634756384757760000000}`,
-    //   (error, stdout, stderr) => {
-    //     if (error) {
-    //       console.error(`exec error: ${error}`);
-    //       return;
+    //     `set`,
+    //     { env: { ...process.env, PERCY_TOKEN: `734573428736548273648723` } },
+    //     (error, stdout, stderr) => {
+    //       if (error) {
+    //         console.error(`exec error: ${error}`);
+    //         return;
+    //       }
+    //       console.log(`stdout: ${stdout}`);
+    //       console.error(`stderr: ${stderr}`);
     //     }
-    //     console.log(`stdout: ${stdout}`);
-    //     console.error(`stderr: ${stderr}`);
-    //   }
-    // );
+    //   );
+    exec(
+      `export PERCY_TOKEN=${345634756384757760000000}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      }
+    );
   }
 };
 resolvePercyTokens();
