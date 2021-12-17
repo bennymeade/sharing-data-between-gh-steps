b87c1d49c7a9e240fd3a6840d2eab8ae3a9c91b9
bennymeade <brendan_meade@yahoo.co.uk>
2021-12-17 09:05:47 +0000

    check in

--- create-env-var.js
+++ create-env-var.js
@@ -1,19 +1,4 @@
-/* eslint-disable no-console */
-const { exec } = require("child_process");
-
 const resolvePercyTokens = () => {
-  // exec(
-  //     `set`,
-  //     { env: { ...process.env, PERCY_TOKEN: `734573428736548273648723` } },
-  //     (error, stdout, stderr) => {
-  //       if (error) {
-  //         console.error(`exec error: ${error}`);
-  //         return;
-  //       }
-  //       console.log(`stdout: ${stdout}`);
-  //       console.error(`stderr: ${stderr}`);
-  //     }
-  //   );
   fs = require("fs");
   fs.writeFile("temptokens.txt", "734573428736548273648723", function (err) {
     if (err) return console.log(err);
